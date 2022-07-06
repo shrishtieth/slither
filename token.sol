9af2a942cc94a9237399e65429a6644f1b14808b
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2022-07-06 17:45:56 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -3,316 +3,8 @@ pragma solidity 0.8.9;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
-
-library Strings {
-    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
-     */
-    function toString(uint256 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
-     */
-    function toHexString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) {
-            return "0x00";
-        }
-        uint256 temp = value;
-        uint256 length = 0;
-        while (temp != 0) {
-            length++;
-            temp >>= 8;
-        }
-        return toHexString(value, length);
-    }
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
-     */
-    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
-        bytes memory buffer = new bytes(2 * length + 2);
-        buffer[0] = "0";
-        buffer[1] = "x";
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
-            buffer[i] = _HEX_SYMBOLS[value & 0xf];
-            value >>= 4;
-        }
-        require(value == 0, "Strings: hex length insufficient");
-        return string(buffer);
-    }
-}
-
-library ECDSA {
-    enum RecoverError {
-        NoError,
-        InvalidSignature,
-        InvalidSignatureLength,
-        InvalidSignatureS,
-        InvalidSignatureV
-    }
-
-    function _throwError(RecoverError error) private pure {
-        if (error == RecoverError.NoError) {
-            return; // no error: do nothing
-        } else if (error == RecoverError.InvalidSignature) {
-            revert("ECDSA: invalid signature");
-        } else if (error == RecoverError.InvalidSignatureLength) {
-            revert("ECDSA: invalid signature length");
-        } else if (error == RecoverError.InvalidSignatureS) {
-            revert("ECDSA: invalid signature 's' value");
-        } else if (error == RecoverError.InvalidSignatureV) {
-            revert("ECDSA: invalid signature 'v' value");
-        }
-    }
-
-    /**
-     * @dev Returns the address that signed a hashed message (`hash`) with
-     * `signature` or error string. This address can then be used for verification purposes.
-     *
-     * The `ecrecover` EVM opcode allows for malleable (non-unique) signatures:
-     * this function rejects them by requiring the `s` value to be in the lower
-     * half order, and the `v` value to be either 27 or 28.
-     *
-     * IMPORTANT: `hash` _must_ be the result of a hash operation for the
-     * verification to be secure: it is possible to craft signatures that
-     * recover to arbitrary addresses for non-hashed data. A safe way to ensure
-     * this is by receiving a hash of the original message (which may otherwise
-     * be too long), and then calling {toEthSignedMessageHash} on it.
-     *
-     * Documentation for signature generation:
-     * - with https://web3js.readthedocs.io/en/v1.3.4/web3-eth-accounts.html#sign[Web3.js]
-     * - with https://docs.ethers.io/v5/api/signer/#Signer-signMessage[ethers]
-     *
-     * _Available since v4.3._
-     */
-    function tryRecover(bytes32 hash, bytes memory signature) internal pure returns (address, RecoverError) {
-        // Check the signature length
-        // - case 65: r,s,v signature (standard)
-        // - case 64: r,vs signature (cf https://eips.ethereum.org/EIPS/eip-2098) _Available since v4.1._
-        if (signature.length == 65) {
-            bytes32 r;
-            bytes32 s;
-            uint8 v;
-            // ecrecover takes the signature parameters, and the only way to get them
-            // currently is to use assembly.
-            assembly {
-                r := mload(add(signature, 0x20))
-                s := mload(add(signature, 0x40))
-                v := byte(0, mload(add(signature, 0x60)))
-            }
-            return tryRecover(hash, v, r, s);
-        } else if (signature.length == 64) {
-            bytes32 r;
-            bytes32 vs;
-            // ecrecover takes the signature parameters, and the only way to get them
-            // currently is to use assembly.
-            assembly {
-                r := mload(add(signature, 0x20))
-                vs := mload(add(signature, 0x40))
-            }
-            return tryRecover(hash, r, vs);
-        } else {
-            return (address(0), RecoverError.InvalidSignatureLength);
-        }
-    }
-
-    /**
-     * @dev Returns the address that signed a hashed message (`hash`) with
-     * `signature`. This address can then be used for verification purposes.
-     *
-     * The `ecrecover` EVM opcode allows for malleable (non-unique) signatures:
-     * this function rejects them by requiring the `s` value to be in the lower
-     * half order, and the `v` value to be either 27 or 28.
-     *
-     * IMPORTANT: `hash` _must_ be the result of a hash operation for the
-     * verification to be secure: it is possible to craft signatures that
-     * recover to arbitrary addresses for non-hashed data. A safe way to ensure
-     * this is by receiving a hash of the original message (which may otherwise
-     * be too long), and then calling {toEthSignedMessageHash} on it.
-     */
-    function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
-        (address recovered, RecoverError error) = tryRecover(hash, signature);
-        _throwError(error);
-        return recovered;
-    }
-
-    /**
-     * @dev Overload of {ECDSA-tryRecover} that receives the `r` and `vs` short-signature fields separately.
-     *
-     * See https://eips.ethereum.org/EIPS/eip-2098[EIP-2098 short signatures]
-     *
-     * _Available since v4.3._
-     */
-    function tryRecover(
-        bytes32 hash,
-        bytes32 r,
-        bytes32 vs
-    ) internal pure returns (address, RecoverError) {
-        bytes32 s = vs & bytes32(0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
-        uint8 v = uint8((uint256(vs) >> 255) + 27);
-        return tryRecover(hash, v, r, s);
-    }
-
-    /**
-     * @dev Overload of {ECDSA-recover} that receives the `r and `vs` short-signature fields separately.
-     *
-     * _Available since v4.2._
-     */
-    function recover(
-        bytes32 hash,
-        bytes32 r,
-        bytes32 vs
-    ) internal pure returns (address) {
-        (address recovered, RecoverError error) = tryRecover(hash, r, vs);
-        _throwError(error);
-        return recovered;
-    }
-
-    /**
-     * @dev Overload of {ECDSA-tryRecover} that receives the `v`,
-     * `r` and `s` signature fields separately.
-     *
-     * _Available since v4.3._
-     */
-    function tryRecover(
-        bytes32 hash,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) internal pure returns (address, RecoverError) {
-        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
-        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
-        // the valid range for s in (301): 0 < s < secp256k1n ÷ 2 + 1, and for v in (302): v ∈ {27, 28}. Most
-        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
-        //
-        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
-        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
-        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
-        // these malleable signatures as well.
-        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
-            return (address(0), RecoverError.InvalidSignatureS);
-        }
-        if (v != 27 && v != 28) {
-            return (address(0), RecoverError.InvalidSignatureV);
-        }
-
-        // If the signature is valid (and not malleable), return the signer address
-        address signer = ecrecover(hash, v, r, s);
-        if (signer == address(0)) {
-            return (address(0), RecoverError.InvalidSignature);
-        }
-
-        return (signer, RecoverError.NoError);
-    }
-
-    /**
-     * @dev Overload of {ECDSA-recover} that receives the `v`,
-     * `r` and `s` signature fields separately.
-     */
-    function recover(
-        bytes32 hash,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) internal pure returns (address) {
-        (address recovered, RecoverError error) = tryRecover(hash, v, r, s);
-        _throwError(error);
-        return recovered;
-    }
-
-    /**
-     * @dev Returns an Ethereum Signed Message, created from a `hash`. This
-     * produces hash corresponding to the one signed with the
-     * https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
-     * JSON-RPC method as part of EIP-191.
-     *
-     * See {recover}.
-     */
-    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32) {
-        // 32 is the length in bytes of hash,
-        // enforced by the type signature above
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    }
-
-    /**
-     * @dev Returns an Ethereum Signed Message, created from `s`. This
-     * produces hash corresponding to the one signed with the
-     * https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
-     * JSON-RPC method as part of EIP-191.
-     *
-     * See {recover}.
-     */
-    function toEthSignedMessageHash(bytes memory s) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n", Strings.toString(s.length), s));
-    }
-
-    /**
-     * @dev Returns an Ethereum Signed Typed Data, created from a
-     * `domainSeparator` and a `structHash`. This produces hash corresponding
-     * to the one signed with the
-     * https://eips.ethereum.org/EIPS/eip-712[`eth_signTypedData`]
-     * JSON-RPC method as part of EIP-712.
-     *
-     * See {recover}.
-     */
-    function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-    }
-}
-
-library Counters {
-    struct Counter {
-        // This variable should never be directly accessed by users of the library: interactions must be restricted to
-        // the library's function. As of Solidity v0.5.2, this cannot be enforced, though there is a proposal to add
-        // this feature: see https://github.com/ethereum/solidity/issues/4637
-        uint256 _value; // default: 0
-    }
-
-    function current(Counter storage counter) internal view returns (uint256) {
-        return counter._value;
-    }
-
-    function increment(Counter storage counter) internal {
-        unchecked {
-            counter._value += 1;
-        }
-    }
-
-    function decrement(Counter storage counter) internal {
-        uint256 value = counter._value;
-        require(value > 0, "Counter: decrement overflow");
-        unchecked {
-            counter._value = value - 1;
-        }
-    }
-
-    function reset(Counter storage counter) internal {
-        counter._value = 0;
-    }
-}
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/ECDSA.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 
 contract TokenDistribution is Ownable{
 
@@ -340,23 +32,27 @@ contract TokenDistribution is Ownable{
     mapping(uint256 => Deposit) public idToDeposit;
     mapping(uint256 => Withdraw) public idToWithdraw;
 
-   
-
    address public signer;
    address payable public treasury;
 
    mapping(uint256 => bool) public nonceUsed;
 
-   event TokensDeposited(address indexed user, uint256 indexed amount, uint256 indexed id);
+   event TokensDeposited(address indexed user, uint256 indexed amount, uint256 indexed id, 
+   address currency);
    event Withdrawn(address indexed user, uint256 indexed amount, uint256 indexed id);
    event CurrencyUpdated(address token, bool canBeUsed);
    event SignerUpdated(address signer);
+   event DevWalletUpdated(address wallet);
+   event BurnWalletUpdated(address wallet);
+   event TreasuryUpdated(address wallet);
 
-   constructor(address coin, address burn, address dev){
+   constructor(address coin, address burn, address dev, address _treasury){
        babyDogeCoin =  coin;
        burnWallet = burn;
        devWallet = dev; 
+       treasury = payable(_treasury);
        canBeUsedAsCurrency[coin] = true;
+       canBeUsedAsCurrency[address(0)] = true;
    }
 
    function updateCurrency(address token, bool canBeUsed) external onlyOwner{
@@ -369,13 +65,31 @@ contract TokenDistribution is Ownable{
        emit SignerUpdated(signer_);
    }
 
+   function updateTreasury(address _treasury) external onlyOwner{
+       treasury = payable(_treasury);
+       emit TreasuryUpdated(_treasury);
+   }
+
+   function updateBurnWallet(address wallet) external onlyOwner{
+       burnWallet = wallet;
+       emit BurnWalletUpdated(wallet);
+   }
+
+   function updateDevWallet(address wallet) external onlyOwner{
+       devWallet = wallet;
+       emit DevWalletUpdated(wallet);
+   }
+
+
+
    function depositTokens(address token, uint256 amount, 
-   uint256 nonce, bytes memory sign ) external payable {
+   uint256 nonce, uint8 v, bytes32 r, bytes32 s ) external payable {
 
      require(canBeUsedAsCurrency[token], "Invalid Currency");
      require(nonceUsed[nonce]==false,"Nonce Used");
      bytes32 hash = hashTransaction(token, nonce, msg.sender, amount, "deposit");
-     require(isValidData(hash, sign),"Invalid Entry");
+     require(isValidData(hash, v, r, s),"Invalid Entry");
+     nonceUsed[nonce] = true;
      if(token == address(0)){
       payable(treasury).transfer(msg.value);
      }
@@ -390,9 +104,10 @@ contract TokenDistribution is Ownable{
       });
       
       depositId.increment();
-      emit TokensDeposited(msg.sender, amount, deposit);
+      emit TokensDeposited(msg.sender, amount, deposit, token);
      
    }
+
 
    function hashTransaction(
         address token,
@@ -410,59 +125,27 @@ contract TokenDistribution is Ownable{
         return hash;
     }
 
-     function isValidData(bytes32 message, bytes memory sig) public view returns(bool){
-       return (recoverSigner(message, sig) == signer);
+     function isValidData(bytes32 message, uint8 v, bytes32 r, bytes32 s) public view returns(bool){
+       return (recoverSigner(message, v,r,s) == signer);
    }
 
-   function recoverSigner(bytes32 message, bytes memory sig)
+   function recoverSigner(bytes32 message,  uint8 v, bytes32 r, bytes32 s)
        public
        pure
        returns (address)
      {
-       uint8 v;
-       bytes32 r;
-       bytes32 s;
-       (v, r, s) = splitSignature(sig);
+      
        return ecrecover(message, v, r, s);
    }
 
-   function splitSignature(bytes memory sig)
-       public
-       pure
-       returns (uint8, bytes32, bytes32)
-     {
-       require(sig.length == 65);
 
-       bytes32 r;
-       bytes32 s;
-       uint8 v;
-
-       assembly {
-           // first 32 bytes, after the length prefix
-           r := mload(add(sig, 32))
-           // second 32 bytes
-           s := mload(add(sig, 64))
-           // final byte (first byte of the next 32 bytes)
-           v := byte(0, mload(add(sig, 96)))
-       }
- 
-       return (v, r, s);
-   }
-
-    function matchSigner(bytes32 hash, bytes memory signature)
-        internal
-        view
-        returns (bool)
-    {
-        return signer == ECDSA.recover(hash, signature);
-    }
 
      function withdrawTokens(uint256 amount, 
-     uint256 nonce, bytes memory sign ) external payable {
+     uint256 nonce, uint8 v, bytes32 r, bytes32 s ) external {
    
      require(nonceUsed[nonce]==false,"Nonce Used");
      bytes32 hash = hashTransaction(babyDogeCoin, nonce, msg.sender, amount, "withdraw");
-     require(isValidData(hash, sign),"Invalid Entry");
+     require(isValidData(hash, v, r, s),"Invalid Entry");
     
       IERC20(babyDogeCoin).transferFrom(treasury, msg.sender, amount);
 
